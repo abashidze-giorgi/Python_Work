0253df8347d81e751e2e9cc570dbdf33a746da43
giorgi abashidze <gabashidze99@gmail.com>
2023-04-18 07:54:18 +0400

    ..

--- data_service.py
+++ data_service.py
@@ -2,13 +2,15 @@ import os
 import datetime
 
 phone_number_len = 11
+
+
 def get_person_age(birth_day: str) -> int:
     age = datetime.datetime.now().year - int(birth_day)
     return age
 
 
 def normalize_phone_number(phone_number: str) -> str:
-    digits = ''
+    digits = "\D, %s, {phone_number}"
     for char in phone_number:
 
         if char.isdigit():
@@ -23,57 +25,58 @@ class DataService:
 
     @staticmethod
     def parse_file(file_path: str, encoding: str) -> dict:
-        combined_list = []
-        cash_h = []
-        pos_h = []
-        
         data = {
-            'cache': [],
-            'pos': []
+            'cash': [],
+            'pos': [],
+            'cards': [],
+            'bad': []
         }
+
         with open(file_path, 'r', encoding=encoding) as f:
             for iter_num, line in enumerate(f.readlines()):
                 person = line.split(";")
-                for zero in range(0, 4):  # удаление 4 пустых значений из листа.
-                    person.remove('')
+
                 """
                 Кривые номера при парсинге должны принтануться в консоли с указанием номера строки где были данные 
                 найдены,
                 а также, именем-отчеством. Формат:"ИО: <имя-отчество>; Телефон: <телефон>".
                 Люди с кривыми номерами никуда записываться не должны.
                 """
-                phone_number = normalize_phone_number(person[0])
+                phone_number = person[0]
+                phone_number = normalize_phone_number(phone_number)
                 if phone_number != '':
-                    birth_year = person[6].split('.')[2]
+                    name = person[3]
+                    birth_year = person[8].split('.')[2]
                     age = get_person_age(birth_year)
+                    full_name = person[4]
+                    pay_method = person[7]
+                    list_len = len(data[pay_method]) + 1
                     person_dict = {
-                        'id': iter_num,
-                        'name': person[1]
+                        'id': list_len,
+                        'name': name,
+                        'fullname': full_name,
+                        'phone': phone_number,
+                        'birth_year': birth_year,
+                        'age': age,
+                        'pay_method': pay_method
                     }
-                    person_dict = dict(id=iter_num, fullname=person[2], phone=phone_number,
-                                       bd=person[6], age=age, salary_pay_method=person[5])
-                    pay_method = person[5]
-                    
                     data[pay_method].append(person_dict)
-                    
                 else:
                     # iter_num+1 потому, что при подсчете строк человек начинает 1, а не с нуля.
-                    name = person[1]
-                    phone = person[0]
-                    invalid_person = ("номер строки:", iter_num + 1, "ИО:", name, "\nТелефон:", phone)
-                    print(invalid_person)
-        combined_list.append(pos_h)
-        combined_list.append(cash_h)
-        print('количество записей в файле - ' + 'pos_h.csv:', len(pos_h))
-        print('количество записей в файле - ' + 'cash_h.csv:', len(cash_h))
-        print('всего записей:', len(cash_h) + len(pos_h))
-        return combined_list
+                    name = person[4]
+                    list_len = len(data['bad']) + 1
+                    invalid_phone_number_person = ("номер строки:", list_len, "ФИО:", name, "\nТелефон:", phone_number)
+                    data['bad'].append(invalid_phone_number_person)
+        print('количество записей в файле - ' + 'pos_h.csv:', len(data['pos']))
+        print('количество записей в файле - ' + 'cash_h.csv:', len(data['cash']))
+        print('количество записей в файле - ' + 'cards_h.csv:', len(data['cards']))
+        print('всего записей:', len(data['pos']) + len(data['cash']) + len(data['cards']))
+        return data
     
     
 #     for p_type, p_data in parse_file().items():
 #         for item in p_data:
 #             print(p_type, item['id])
-    
 
     """5. По завершении парсинга файла и сохранения данных в файлы, в консоль, в табличном виде, должны принтануться 
     следующие данные: 5.1. Кол-во не уникальных номеров телефонов, и сами номера 5.2. Статистика по людям: сколько 
@@ -99,18 +102,15 @@ class DataService:
                 print("Person id", val[1].split(",")[0].split(":")[1], "Phone number:",
                       val[1].split(",")[3].split(":")[1])
 
-
-    def diversification_data_and_save_files(data: list[list[dict[str, str | int]]], encoding: str) -> list():
+    def diversification_data_and_save_files(self: dict, encoding: str) -> list():
         file_names = []
 
-        for datalist in data:
-            cur_id = 1
+        for data_list in self\
+                :
             filename = os.path.join(os.path.expanduser('~'), 'Documents', 'my_output_folder',
-                                    datalist[0]["salary_pay_method"]+'_h.csv')
+                                    datalist + '_h.csv')
             file_names.append(filename)
             with open(filename, 'w', encoding=encoding) as f:
                 for item in datalist:
-                    item["id"] = cur_id
                     f.write("%s\n" % item)
-                    cur_id += 1
         return file_names
